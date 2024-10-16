477f0fc355a69f9a7b26b66a48c9c7fea244a56f
anayak2kafka <anayak2@buffalo.edu>
2024-10-15 22:54:52 -0400

    Initial commit

---
+++ HW2/A1Q1/A1Q1/a1.hpp
@@ -0,0 +1,16 @@
+/*  YOUR_FIRST_NAME
+ *  YOUR_LAST_NAME
+ *  YOUR_UBIT_NAME
+ */
+
+#ifndef A1_HPP
+#define A1_HPP
+
+#include <vector>
+
+
+void filter2d(long long int n, long long int m, const std::vector<float>& K, std::vector<float>& A) {
+
+} // filter2d
+
+#endif // A1_HPP
